476baed01f17fb89f445815405b0bdbbe6335b88
Mihir0217 <mihirmistry1702@gmail.com>
2024-12-26 20:17:45 +0530

    serch bar added but not the backend part

--- obj/Debug/net9.0/firstmvc.AssemblyInfo.cs
+++ obj/Debug/net9.0/firstmvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firstmvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e64dcc2004d20bd575e55b58a21a197bd59cb2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("firstmvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firstmvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
